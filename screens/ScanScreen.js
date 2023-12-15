785dff92436b88e88092a5a944ea25fae3eda7e5
Resources-DTW <resource.dtw@gmail.com>
2023-12-15 19:14:03 +0530

    init

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,27 +1,8 @@
-import { View, Text, TouchableOpacity, Animated, Easing } from "react-native";
-import React, { useEffect, useState } from "react";
-// import LottieView from "lottie-react-native";
-import Svg, { Circle, Line } from "react-native-svg";
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import LottieView from "lottie-react-native";
 
 const ScanScreen = ({ navigation }) => {
-  const [rotation] = useState(new Animated.Value(0));
-
-  useEffect(() => {
-    Animated.loop(
-      Animated.timing(rotation, {
-        toValue: 1,
-        duration: 2000,
-        easing: Easing.linear,
-        useNativeDriver: true,
-      })
-    ).start();
-  }, [rotation]);
-
-  const rotate = rotation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ["0deg", "360deg"],
-  });
-
   return (
     <View className="items-center justify-center flex-1 pl-[20%] pr-[20%]">
       <View className="absolute top-[15%]">
@@ -32,30 +13,12 @@ const ScanScreen = ({ navigation }) => {
           Find your Bluetooth devices
         </Text>
         <View className="flex-1 items-center justify-center">
-          {/* <LottieView
+          <LottieView
+            className="w-[100%] h-[100%]"
             source={require("../assets/bleScan.json")}
             autoPlay
             loop
-          /> */}
-          <Svg height="100%" width="100%" viewBox="0 0 100 100">
-            <Circle
-              cx={50}
-              cy={50}
-              r={45}
-              stroke="black"
-              strokeWidth={2}
-              fill="white"
-            />
-            <Line
-              x1={50}
-              y1={5}
-              x2={50}
-              y2={15}
-              stroke="black"
-              strokeWidth={2}
-              transform={`rotate(${rotate} 50 50)`}
-            />
-          </Svg>
+          />
         </View>
       </View>
       <TouchableOpacity
